edfb9f08e1eb99806965dc42ba4735a74996c01b
Henry Chu <henrytech@outlook.com>
2022-02-15 12:02:09 +0800

    switch jetbrains runtime to jcef

--- Casks/jetbrains-runtime.rb
+++ Casks/jetbrains-runtime.rb
@@ -1,13 +1,13 @@
 cask "jetbrains-runtime" do
   version "17.0.2,315.1"
-  sha256 "9ca077493d6f3fc0cb9194548ca80518cdcd69430734b5f34dde17361d05af0a"
+  sha256 "d26deea2e1567d345e96f504b97b194dd015592a76d5260f9cc0589313699489"
 
-  url "https://cache-redirector.jetbrains.com/intellij-jbr/jbrsdk-#{version.csv.first.dots_to_underscores}-osx-aarch64-b#{version.csv.second}.pkg"
+  url "https://cache-redirector.jetbrains.com/intellij-jbr/jbrsdk_jcef-#{version.csv.first.dots_to_underscores}-osx-aarch64-b#{version.csv.second}.pkg"
   name "jetbrains-runtime"
   desc "Runtime environment based on OpenJDK for running JetBrains products"
   homepage "https://github.com/JetBrains/JetBrainsRuntime"
 
-  pkg "jbrsdk-#{version.before_comma.dots_to_underscores}-osx-aarch64-b#{version.after_comma}.pkg"
+  pkg "jbrsdk_jcef-#{version.before_comma.dots_to_underscores}-osx-aarch64-b#{version.after_comma}.pkg"
 
   uninstall pkgutil: "com.jetbrains.jbrsdk"
 end
