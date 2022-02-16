ca755d925c2d41d2eb034d7be26f02a536e9b12e
Henry Chu <henrytech@outlook.com>
2022-02-16 09:54:17 +0800

    corrected name of jetbrains runtime

--- Casks/jetbrains-runtime.rb
+++ Casks/jetbrains-runtime.rb
@@ -3,7 +3,7 @@ cask "jetbrains-runtime" do
   sha256 "d26deea2e1567d345e96f504b97b194dd015592a76d5260f9cc0589313699489"
 
   url "https://cache-redirector.jetbrains.com/intellij-jbr/jbrsdk_jcef-#{version.csv.first.dots_to_underscores}-osx-aarch64-b#{version.csv.second}.pkg"
-  name "jetbrains-runtime"
+  name "JetBrains Runtime"
   desc "Runtime environment based on OpenJDK for running JetBrains products"
   homepage "https://github.com/JetBrains/JetBrainsRuntime"
 
