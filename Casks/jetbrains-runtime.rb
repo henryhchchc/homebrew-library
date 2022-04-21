2e13e2487a47a176dba2a39900884ebb2b241f4e
Henry Chu <henrytech@outlook.com>
2022-04-21 12:37:08 +0800

    Update jetbrains-runtime from 17.0.2,315.1 to 17.0.2,396.4

--- Casks/jetbrains-runtime.rb
+++ Casks/jetbrains-runtime.rb
@@ -1,13 +1,13 @@
 cask "jetbrains-runtime" do
-  version "17.0.2,315.1"
-  sha256 "d26deea2e1567d345e96f504b97b194dd015592a76d5260f9cc0589313699489"
+  version "17.0.2,396.4"
+  sha256 "b0ab3a420252f86249f504779db5726db69d35aa1cf8c089d3d47c2d348973ff"
 
-  url "https://cache-redirector.jetbrains.com/intellij-jbr/jbrsdk_jcef-#{version.csv.first.dots_to_underscores}-osx-aarch64-b#{version.csv.second}.pkg"
+  url "https://cache-redirector.jetbrains.com/intellij-jbr/jbrsdk_jcef-#{version.csv.first}-osx-aarch64-b#{version.csv.second}.pkg"
   name "JetBrains Runtime"
   desc "Runtime environment based on OpenJDK for running JetBrains products"
   homepage "https://github.com/JetBrains/JetBrainsRuntime"
 
-  pkg "jbrsdk_jcef-#{version.before_comma.dots_to_underscores}-osx-aarch64-b#{version.after_comma}.pkg"
+  pkg "jbrsdk_jcef-#{version.before_comma}-osx-aarch64-b#{version.after_comma}.pkg"
 
   uninstall pkgutil: "com.jetbrains.jbrsdk"
 end
