50823e5639379ee62961699887363621b14b5c21
Henry Chu <henrytech@outlook.com>
2022-04-22 10:20:22 +0800

    Update adobe-acrobat-font-pack from 21.007.20091 to 22.001.20085

--- Casks/adobe-acrobat-font-pack.rb
+++ Casks/adobe-acrobat-font-pack.rb
@@ -1,6 +1,6 @@
 cask "adobe-acrobat-font-pack" do
-  version "21.007.20091"
-  sha256 "3fb67cc3140de22bde3beb9483fce484992c810ff4cf63c7f0c943cf861a3dd8"
+  version "22.001.20085"
+  sha256 "d485dd59eb5097e207e4f10c73dc72ed686157c9527906edeabe576ffa3f7adc"
 
   url "https://ardownload3.adobe.com/pub/adobe/reader/mac/AcrobatDC/misc/FontPack#{version.no_dots}_XtdAlf_Lang_DC.dmg"
   name "Font Pack fro Adobe Acrobat"
@@ -12,6 +12,6 @@ cask "adobe-acrobat-font-pack" do
   uninstall pkgutil: "com.adobe.Reader.fontpack.DC.Xtd_Lang.pkg"
 
   caveats <<~EOS
-      This cask will install and work only when adobe-acrobat-reader or adobe-acrobat-pro is installed.
-    EOS
+    This cask will install and work only when adobe-acrobat-reader or adobe-acrobat-pro is installed.
+  EOS
 end
